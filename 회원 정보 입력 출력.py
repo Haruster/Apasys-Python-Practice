d951401d0d211ac03a616ee7a0f8b94f9354b342
Apasys <67867993+Apasys@users.noreply.github.com>
2021-09-28 02:13:08 +0900

    Create 회원 정보 입력 출력.py

---
+++ 회원 정보 입력 출력.py
@@ -0,0 +1,34 @@
+usr_name = input("당신의 이름을 입력해주세요 : ")
+
+print("")
+
+usr_email = input("당신이 사용하는 이메일을 적어주세요 : ")
+
+print("")
+
+usr_id = input("당신이 사용하실 아이디를 입력해주세요 : ")
+
+print("")
+
+usr_pw = input("당신이 사용할 비밀번호를 입력해주세요 : ")
+
+print("")
+
+str_none = "----------------------------------------------------------------------------------------------------"
+
+print("정보 등록 중 입니다 ..............")
+
+print(str_none)
+
+print("To.", usr_email) #user email 출력
+
+print(usr_name, "님 반갑습니다.") # user name 출력
+
+print("당신이 설정하신 ID와 PW는 다음과 같습니다.")
+
+print(str_none)
+
+print("ID : ", usr_id) #user id 출력
+print("PW : ", usr_pw) #user pw 출력
+
+
